0b646e8e4d8fa928aa7d3f3526b87f3dd120508d
adnan wahab <mail@adnanwahab.com>
2021-04-06 15:33:36 -0500

    wtf

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -27,14 +27,16 @@ function connectToDatabase(uri) {
   })
 }
 
-function recordView(db, document) {
+function recordView(db, host) {
   //{ item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } }
-  db.inventory.insertOne({
-    item: "canvas",
-    qty: 100,
-    tags: ["cotton"],
-    size: { h: 28, w: 35.5, uom: "cm" },
-  })
+  db.db("analytics")
+    .collection("product_views")
+    .insertOne({
+      item: "view",
+      host: host,
+      tags: ["cotton"],
+      size: { h: 28, w: 35.5, uom: "cm" },
+    })
 }
 
 function queryDatabase(db, collection) {
@@ -47,29 +49,32 @@ function queryDatabase(db, collection) {
     image_url: 1,
     _id: 0,
   }
-  return db
-    .db("analytics")
-    .collection("product_views")
-    .find({})
-    .project(project)
-    .toArray()
-    .then((result) => {
-      return result
-    })
-    .catch((err) => {
-      console.log("=> an error occurred: ", err)
-      return { statusCode: 500, body: "error" }
-    })
+  return (
+    db
+      .db("analytics")
+      .collection("product_views")
+      .find({})
+      //.project(project)
+      .toArray()
+      .then((result) => {
+        return result
+      })
+      .catch((err) => {
+        console.log("=> an error occurred: ", err)
+        return { statusCode: 500, body: "error" }
+      })
+  )
 }
 console.log("aaaa")
 
 export default (req, res) => {
+  console.log(req.headers["x-vercel-id"])
+  console.log(req.headers["x-vercel-signature"])
   if (req.method === "POST") {
   }
 
-  console.log(req)
   connectToDatabase(MONGODB_URI)
-    .then(recordView)
+    .then((_) => recordView(_, req.headers.host))
     .then((result) => res.status(200).json({ success: "yay" }))
   //
   //     .then((db) => queryDatabase(db))
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,20 @@
 /** @format */
-
+import { useState, useEffect } from "react"
 import Head from "next/head"
 /*  */ import styles from "../styles/Home.module.css"
 
 import "tailwindcss/tailwind.css"
 
 export default function Dashboard(titites) {
+  let [res, setRes] = useState()
+
+  useEffect(() => {
+    ;(async () => {
+      let res = await fetch("/api/hello")
+      let json = await res.json()
+      setRes(setRes)
+    })()
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +24,7 @@ export default function Dashboard(titites) {
 
       <div className='antialiased'></div>
       <h1>Analytics go here :)))))</h1>
+      <div>{res}</div>
     </div>
   )
 }
